177d5337b9f0601c6e034e88cd1f88ca82bb070e
Yue Zhou <26257959+mrwrongwrongwrong@users.noreply.github.com>
2021-10-11 22:34:07 -0400

    Add files via upload

---
+++ problem4-ambulance/第二步，用退火算法找最短路径.py
@@ -0,0 +1,120 @@
+import json
+# from random import choice
+import random
+from hps.clients import SocketClient
+from time import sleep
+import argparse
+from copy import deepcopy
+import time
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import math
+import random
+
+
+#assume that the given points on the map are in the format of list of list, so we can take input as a list, then start implement
+# creating data
+#https://www.geeksforgeeks.org/ml-k-means-algorithm/
+mean_01 = np.array([0.0, 0.0])
+cov_01 = np.array([[1, 0.3], [0.3, 1]])
+dist_01 = np.random.multivariate_normal(mean_01, cov_01, 100)
+
+mean_02 = np.array([6.0, 7.0])
+cov_02 = np.array([[1.5, 0.3], [0.3, 1]])
+dist_02 = np.random.multivariate_normal(mean_02, cov_02, 100)
+
+mean_03 = np.array([7.0, -5.0])
+cov_03 = np.array([[1.2, 0.5], [0.5, 1, 3]])
+dist_03 = np.random.multivariate_normal(mean_03, cov_01, 100)
+
+mean_04 = np.array([2.0, -7.0])
+cov_04 = np.array([[1.2, 0.5], [0.5, 1, 3]])
+dist_04 = np.random.multivariate_normal(mean_04, cov_01, 100)
+
+data = np.vstack((dist_01, dist_02, dist_03, dist_04))
+np.random.shuffle(data)
+
+print(data)
+#OK, so far we have verified our assumption, the input data format is a list of a list, each list inside of the entire list is the cordination location
+
+# function to plot the selected centroids
+def plot(data, centroids):
+    plt.scatter(data[:, 0], data[:, 1], marker='.',
+                color='gray', label='data points')
+    plt.scatter(centroids[:-1, 0], centroids[:-1, 1],
+                color='black', label='previously selected centroids')
+    plt.scatter(centroids[-1, 0], centroids[-1, 1],
+                color='red', label='next centroid')
+    plt.title('Select % d th centroid' % (centroids.shape[0]))
+
+    plt.legend()
+    plt.xlim(-5, 12)
+    plt.ylim(-10, 15)
+    plt.show()
+
+
+# function to compute euclidean distance
+def distance(p1, p2):
+    return np.sum((p1 - p2) ** 2)
+
+
+# initialization algorithm
+def initialize(data, k):
+    '''
+    initialized the centroids for K-means++
+    inputs:
+        data - numpy array of data points having shape (200, 2)
+        k - number of clusters
+    '''
+    ## initialize the centroids list and add
+    ## a randomly selected data point to the list
+    centroids = []
+    centroids.append(data[np.random.randint(
+        data.shape[0]), :])
+    print('the centroids locations are',np.array(centroids))
+
+    ## compute remaining k - 1 centroids
+    for c_id in range(k - 1):
+
+        ## initialize a list to store distances of data
+        ## points from nearest centroid
+        dist = []
+        for i in range(data.shape[0]):
+            point = data[i, :]
+            d = sys.maxsize
+
+            ## compute distance of 'point' from each of the previously
+            ## selected centroid and store the minimum distance
+            for j in range(len(centroids)):
+                temp_dist = distance(point, centroids[j])
+                d = min(d, temp_dist)
+            dist.append(d)
+
+        ## select data point with maximum distance as our next centroid
+        dist = np.array(dist)
+        next_centroid = data[np.argmax(dist), :]
+        centroids.append(next_centroid)
+        dist = []
+        print('the centroid locations are',np.array(centroids))
+    return centroids
+
+
+# call the initialize function to get the centroids
+centroids = initialize(data, k=4)
+#careful, the data has to be in the format of [[][][]]
+#https://www.cnblogs.com/heaad/archive/2010/12/20/1911614.html
+#https://github.com/PaulCombal/vrp-wesh/blob/master/sa.py
+
+#https://developers.google.com/optimization/routing/vrp
+
+#退火算法 vpr(vehicle routing problem)
+#https://www.google.com/search?q=%E9%80%80%E7%81%AB%E7%AE%97%E6%B3%95+vrp&oq=%E9%80%80%E7%81%AB%E7%AE%97%E6%B3%95+vrp&aqs=chrome..69i57.5522j0j4&sourceid=chrome&ie=UTF-8
+#https://github.com/lzane/VRP-using-SA-with-Matlab
+#https://zhuanlan.zhihu.com/p/157570311
+#https://blog.csdn.net/python_n/article/details/114416957
+
+#good
+#https://blog.csdn.net/tangshishe/article/details/115495609
